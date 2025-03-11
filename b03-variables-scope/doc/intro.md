d06ee76b328a9ef4fe2d6be4de01ca1fc945c92c
saing sab <saing.s-external@webtiga.com>
2025-03-11 15:54:55 +0700

    init

---
+++ b03-variables-scope/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to b03-variables-scope
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ b03-variables-scope/test/b03_variables_scope/core_test.clj
@@ -0,0 +1,7 @@
+(ns b03-variables-scope.core-test
+  (:require [clojure.test :refer :all]
+            [b03-variables-scope.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
